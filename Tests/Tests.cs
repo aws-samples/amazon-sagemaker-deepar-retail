2ecdfa19d8b8cdfc531518c653be074f0491e994
Paul Fryer <paulfryer@macrowebservices.com>
2018-12-21 06:13:03 -0800

    applying copyright.

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,3 +1,6 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using System;
 using System.Collections.Generic;
 using System.IO;
